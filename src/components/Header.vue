f2f219f89944ac1defc23c1b580f9699bf25edbe
HarryGao1218 <harry011218@sina,com>
2022-07-12 11:52:48 +0800

    小逝一下

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div style="height:50px;line-height: 50px;border-bottom: 1px solid #ccc;display: flex">
+    <div style="width: 200px;padding-left: 30px;font-weight: bold;color: dodgerblue">FzuJGXY</div>
+    <div style="flex: 1"></div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Head"
+}
+</script>
+
+<style scoped>
+
+</style>
